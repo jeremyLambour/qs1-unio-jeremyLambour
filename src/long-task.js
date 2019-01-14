07fae8d11c0fe91da2d22947f3f9b7650e92657e
jeremy.lambour <jeremy.lambour@gmail.com>
2019-01-14 22:30:20 +0100

    update long-task.js

--- src/long-task.js
+++ src/long-task.js
@@ -1,6 +1,7 @@
-const heavyDo = () => {
+const callstack = [];
+const notSoHeavyDo = times => {
   let count = 0;
-  for (let i = 0; i < 1e8; i++) {
+  for (let i = 0; i < times; i++) {
     if (
       Math.round(
         Math.log(Math.sqrt(Math.abs(Math.round(Math.random() * 1e10))))
@@ -11,7 +12,35 @@ const heavyDo = () => {
   return count;
 };
 
-let interval = setInterval(() => console.log('I am blocked'), 1000);
-console.log(heavyDo()); // take almost 10 seconds
+const heavyDo = callback => {
+  let total = 1e8,
+    cuts = 100,
+    counts = 0;
+  for (let i = 0; i < cuts; i++) {
+    callstack.push(() => {
+      return notSoHeavyDo(total / cuts);
+    });
+  }
+  executeProcess(counts, callback);
+};
 
+/**
+ * execute subpart of long task
+ * @param {*} count
+ * @param {*} cb final callback after long task
+ */
+const executeProcess = (count, cb) => {
+  const subprocess = callstack.shift();
+  count = count + subprocess();
+  setImmediate(() => {
+    if (callstack.length != 0) {
+      executeProcess(count, cb);
+    } else {
+      cb(count);
+    }
+  });
+};
+
+const interval = setInterval(() => console.log('I am not blocked'), 1000);
 setTimeout(() => clearInterval(interval), 10000);
+heavyDo(counts => console.log(counts));
